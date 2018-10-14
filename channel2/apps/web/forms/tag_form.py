15ca5d997f65445a4062bf302722e0408d312a26
Derek Kwok <dkwok24@gmail.com>
2018-10-14 10:19:38 -0700

    Implement Tag form backend.

---
+++ channel2/apps/web/forms/tag_form.py
@@ -0,0 +1,77 @@
+from django import forms
+
+from channel2.apps.data.models import tag as tag_module
+
+ERROR_NAME_HAS_COMMA = 'The tag name must not contain a comma.'
+ERROR_MISSING_TAGS = 'Unable to find tag(s): {}.'
+
+class TagForm(forms.ModelForm):
+
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Name',
+            'required': 'required',
+        })
+    )
+
+    children = forms.CharField(
+        required=False,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Children Tags',
+        })
+    )
+
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(attrs={
+            'placeholder': 'Description',
+        })
+    )
+
+    class Meta:
+        model = tag_module.Tag
+        fields = ('name', 'type', 'description')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.label_suffix = ''
+        self.children_tag_models = set()
+
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        if ',' in name:
+            raise forms.ValidationError(ERROR_NAME_HAS_COMMA)
+        return name
+
+    def clean_children(self):
+        children = self.cleaned_data.get('children')
+        tag_names = set()
+        for tag_name in children.split(','):
+            tag_name = tag_name.strip()
+            if tag_name:
+                tag_names.add(tag_name)
+
+        tag_models = tag_module.Tag.objects.filter(name__in=tag_names)
+        missing_tag_names = tag_names - set(t.name for t in tag_models)
+        if missing_tag_names:
+            error_message = ERROR_MISSING_TAGS.format(', '.join(sorted(missing_tag_names)))
+            raise forms.ValidationError(error_message)
+
+        self.children_tag_models = tag_models
+        return children
+
+    def save(self, commit=True):
+        tag = super().save(commit=False)
+        if commit:
+            tag.save()
+
+        # Update children tags.
+        cur_tags = set(tag.children.all())
+        new_tags = set(self.children_tag_models)
+        for child_tag in cur_tags - new_tags:
+            tag_module.TagChildren.objects.get(parent=tag, child=child_tag).delete()
+        for child_tag in new_tags - cur_tags:
+            tag_module.TagChildren.objects.create(parent=tag, child=child_tag)
+
+        return tag
---
+++ channel2/apps/web/forms/tag_form_test.py
@@ -0,0 +1,67 @@
+from django import forms, test
+
+from channel2.apps.data.models import tag as tag_module
+from channel2.apps.web.forms import tag_form
+
+
+class TagFormTest(test.TestCase):
+
+    def test_clean_name(self):
+        form = tag_form.TagForm()
+        form.cleaned_data = {'name': 'Hello, World!'}
+        with self.assertRaises(forms.ValidationError) as context_manager:
+            form.clean_name()
+        self.assertEqual(context_manager.exception.message, tag_form.ERROR_NAME_HAS_COMMA)
+
+    def test_clean_children(self):
+        tag1 = tag_module.Tag.objects.create(name='tag 1')
+        tag2 = tag_module.Tag.objects.create(name='tag 2')
+        form = tag_form.TagForm()
+        form.cleaned_data = {'children': 'tag 1 , tag 2,'}
+        form.clean_children()
+        self.assertCountEqual(form.children_tag_models, [tag1, tag2])
+
+    def test_clean_children_missing_tag(self):
+        tag_module.Tag.objects.create(name='tag 1')
+        form = tag_form.TagForm()
+        form.cleaned_data = {'children': 'tag 1 , tag 2, tag 3'}
+        with self.assertRaises(forms.ValidationError) as context_manager:
+            form.clean_children()
+        self.assertEqual(
+            context_manager.exception.message,
+            tag_form.ERROR_MISSING_TAGS.format('tag 2, tag 3'))
+
+    def test_save_create(self):
+        tag1 = tag_module.Tag.objects.create(name='tag 1')
+        tag2 = tag_module.Tag.objects.create(name='tag 2')
+
+        form = tag_form.TagForm(data={
+            'name': 'tag 0',
+            'type': tag_module.TagType.ANIME,
+            'children': 'tag 1, tag 2',
+            'description': 'Tag 0 description.',
+        })
+
+        self.assertTrue(form.is_valid(), form.errors)
+        tag = form.save()
+        self.assertEqual(tag.name, 'tag 0')
+        self.assertEqual(tag.description, 'Tag 0 description.')
+        self.assertCountEqual(tag.children.all(), [tag1, tag2])
+
+    def test_save_edit(self):
+        child_tag1 = tag_module.Tag.objects.create(name='child tag 1')
+        child_tag2 = tag_module.Tag.objects.create(name='child tag 2')
+        parent_tag = tag_module.Tag.objects.create(name='parent tag')
+        tag_module.TagChildren.objects.create(parent=parent_tag, child=child_tag1)
+
+        form = tag_form.TagForm(instance=parent_tag, data={
+            'name': 'parent tag updated',
+            'type': tag_module.TagType.ANIME,
+            'children': 'child tag 2',
+            'description': '',
+        })
+
+        self.assertTrue(form.is_valid())
+        parent_tag = form.save()
+        self.assertEqual(parent_tag.name, 'parent tag updated')
+        self.assertCountEqual(parent_tag.children.all(), [child_tag2])
